d0d5d4ea51cac26ada3fc5e7368d212ac0c84e75
Pablo David <castillo.morales.pablo.prog@gmail.com>
2019-01-27 01:36:48 -0600

    register created

--- script.js
+++ script.js
@@ -8,22 +8,74 @@ storageBucket: "gs://chat-firebase-50a59.appspot.com",
 };
 firebase.initializeApp(config);
 
+firebase.auth().onAuthStateChanged(user=>{
+  console.log(user)
+  if (user) {
+    if(!user.displayName){
+      user.updateProfile({
+          displayName:document.getElementById("name").value,
+          photoURL: document.getElementById("urlimg").value
+      }).then(function() {
+        // Update successful.
+      }).catch(function(error) {
+        console.log(error)
+      });
+    }
+
+    showchat()
+  } else {
+    showlogin()
+  }
+});
+
+let signin=()=>{
+  var email=document.getElementById("email2").value;
+  var password=document.getElementById("password2").value;
+  firebase.auth().createUserWithEmailAndPassword(email,password).catch(
+    e=>console.log(e)
+  )
+}
+
 let login=()=>{
   var email=document.getElementById('email').value
   var password=document.getElementById('password').value
-  firebase.auth().signInWithEmailAndPassword(email,password).catch((e)=>{
+  firebase.auth().signInWithEmailAndPassword(email,password).catch(e=>{
     console.log(e.message);
     console.log(e.code)
   });
 }
-let logout=()=>{
-  firebase.auth().signOut();
-}
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log(user)
-    document.getElementById("login").classList.add("sethide")
-    document.getElementById("chat").classList.remove("sethide")
-  } else {
+let logout=()=> firebase.auth().signOut();
+
+let showlogin=()=>{
+  if(document.getElementById("login").classList.contains("sethide")){
+    document.getElementById("login").classList.remove("sethide")
   }
-});
+  if(!document.getElementById("signin").classList.contains("sethide")){
+    document.getElementById("signin").classList.add("sethide")
+  }
+  if(!document.getElementById("chat").classList.contains("sethide")){
+    document.getElementById("chat").classList.add("sethide")
+  }
+}
+let showsignin=()=>{
+  if(!document.getElementById("login").classList.contains("sethide")){
+    document.getElementById("login").classList.add("sethide")
+  }
+  if(document.getElementById("signin").classList.contains("sethide")){
+    document.getElementById("signin").classList.remove("sethide")
+  }
+  if(!document.getElementById("chat").classList.contains("sethide")){
+    document.getElementById("chat").classList.add("sethide")
+  }
+}
+let showchat=()=>{
+  if(!document.getElementById("login").classList.contains("sethide")){
+    document.getElementById("login").classList.add("sethide")
+  }
+  if(!document.getElementById("signin").classList.contains("sethide")){
+    document.getElementById("signin").classList.add("sethide")
+  }
+  if(document.getElementById("chat").classList.contains("sethide")){
+    document.getElementById("chat").classList.remove("sethide")
+  }
+}
